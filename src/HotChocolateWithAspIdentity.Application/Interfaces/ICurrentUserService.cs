b560fad1f548952940baaa50c7d5cadaa15f073f
Alex Watts <alexjwatts83@gmail.com>
2022-05-25 07:28:34 +1000

    WIP adding IApplicationDbContext

--- src/HotChocolateWithAspIdentity.Application/Interfaces/ICurrentUserService.cs
+++ src/HotChocolateWithAspIdentity.Application/Interfaces/ICurrentUserService.cs
@@ -1,9 +1,20 @@
-﻿namespace HotChocolateWithAspIdentity.Application.Interfaces
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotChocolateWithAspIdentity.Application.Interfaces
 {
 	public interface ICurrentUserService
 	{
 		string UserId { get; }
 
 		// Extend with required data
+	}
+
+	public interface IApplicationDbContext
+	{
+		DbSet<TodoList> TodoLists { get; set; }
+
+		DbSet<TodoItem> TodoItems { get; set; }
+
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 	}
 }
