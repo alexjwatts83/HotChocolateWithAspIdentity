0b2e1e93b1b180c14a5261b1c4cb5f9c5c6b2f86
Alex Watts <alexjwatts83@gmail.com>
2022-05-27 07:57:47 +1000

    added empty ConfigureServices

---
+++ src/HotChocolateWithAspIdentity.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,37 @@
+﻿using HotChocolateWithAspIdentity.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HotChocolateWithAspIdentity.Infrastructure
+{
+	public static class ConfigureServices
+	{
+		public static IServiceCollection AddInfrastructureServices(
+			this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			//services.AddDbContext<ApplicationDbContext>(options =>
+			//	options.UseSqlServer(
+			//		configuration.GetConnectionString("DefaultConnection")));
+
+			//services.AddScoped<IApplicationDbContext>(provider =>
+			//	provider.GetService<ApplicationDbContext>());
+
+			//services.AddTransient<ApplicationDbContextInitialiser>();
+
+			//services.AddDefaultIdentity<ApplicationUser>()
+			//	.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			//services.AddIdentityServer()
+			//	.AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+			//services.AddAuthentication()
+			//	.AddIdentityServerJwt();
+
+			//services.AddScoped<IIdentityService, IdentityService>();
+
+			return services;
+		}
+	}
+}
