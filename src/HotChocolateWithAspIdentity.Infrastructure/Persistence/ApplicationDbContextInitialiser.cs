e640e5c86acfee9a65b15c8d57b3e30b8960a2e9
Alex Watts <alexjwatts83@gmail.com>
2022-05-28 11:39:44 +1000

    wip ApplicationDbContextInitialiser

--- src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
+++ src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -2,6 +2,11 @@
 {
 	public class ApplicationDbContextInitialiser
 	{
+		private readonly ApplicationDbContext _context;
 
+		public ApplicationDbContextInitialiser(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 	}
 }
