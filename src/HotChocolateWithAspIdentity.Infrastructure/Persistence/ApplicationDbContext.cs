b25629ecf198b0501d2671fa0221f39088857cee
Alex Watts <alexjwatts83@gmail.com>
2022-05-28 11:35:57 +1000

    removed usings

--- src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -7,11 +7,7 @@ using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.Infrastructure.Persistence
 {
