bb7c8454be77be9a34531e8abbfada9e8bef37ef
Alex Watts <alexjwatts83@gmail.com>
2022-05-28 11:35:42 +1000

    Filled out ApplicationDbContext

--- src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/HotChocolateWithAspIdentity.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using HotChocolateWithAspIdentity.Application.Interfaces;
+using HotChocolateWithAspIdentity.Domain.Entities;
+using HotChocolateWithAspIdentity.Infrastructure.Identity;
+using HotChocolateWithAspIdentity.Infrastructure.Persistence.Interceptors;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.Infrastructure.Persistence
 {
-	class ApplicationDbContext
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
 	{
+		private readonly AuditEntitiesSaveChangesInterceptor _auditEntitiesSaveChangesInterceptor;
+
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions,
+			ICurrentUserService currentUserService) : base(options, operationalStoreOptions)
+		{
+			_auditEntitiesSaveChangesInterceptor = new AuditEntitiesSaveChangesInterceptor(currentUserService);
+		}
+
+		public DbSet<TodoList> TodoLists { get; set; }
+
+		public DbSet<TodoItem> TodoItems { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder
+				.EnableDetailedErrors()
+				.LogTo(Console.WriteLine);
+
+			optionsBuilder.AddInterceptors(_auditEntitiesSaveChangesInterceptor);
+
+			base.OnConfiguring(optionsBuilder);
+		}
 	}
 }
