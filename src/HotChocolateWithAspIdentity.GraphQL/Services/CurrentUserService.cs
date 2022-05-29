7214a4aaf8db519ccfc2dd8fccce69f52bfae3b5
Alex Watts <alexjwatts83@gmail.com>
2022-05-30 09:09:16 +1000

    Added empty CurrentUserService

---
+++ src/HotChocolateWithAspIdentity.GraphQL/Services/CurrentUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotChocolateWithAspIdentity.GraphQL.Services
+{
+	public class CurrentUserService
+	{
+	}
+}
