d234086a3c9a4bd4c1f641337e31d5053a8dd2e7
Alex Watts <alexjwatts83@gmail.com>
2022-05-29 11:04:48 +1000

    wip ConfigureServices

--- src/HotChocolateWithAspIdentity.Infrastructure/ConfigureServices.cs
+++ src/HotChocolateWithAspIdentity.Infrastructure/ConfigureServices.cs
@@ -1,4 +1,6 @@
 ﻿using HotChocolateWithAspIdentity.Application.Interfaces;
+using HotChocolateWithAspIdentity.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -11,9 +13,9 @@ namespace HotChocolateWithAspIdentity.Infrastructure
 			this IServiceCollection services,
 			IConfiguration configuration)
 		{
-			//services.AddDbContext<ApplicationDbContext>(options =>
-			//	options.UseSqlServer(
-			//		configuration.GetConnectionString("DefaultConnection")));
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(
+					configuration.GetConnectionString("DefaultConnection")));
 
 			//services.AddScoped<IApplicationDbContext>(provider =>
 			//	provider.GetService<ApplicationDbContext>());
