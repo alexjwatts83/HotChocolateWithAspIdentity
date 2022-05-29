c2447ee096c59261fe3fa8ad3f2b73b3462bb3db
Alex Watts <alexjwatts83@gmail.com>
2022-05-30 09:10:30 +1000

    filled out CurrentUserService

--- src/HotChocolateWithAspIdentity.GraphQL/Services/CurrentUserService.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/Services/CurrentUserService.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using HotChocolateWithAspIdentity.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.Services
 {
-	public class CurrentUserService
+	public class CurrentUserService: ICurrentUserService
 	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+		}
+
+		public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
 	}
+
+
 }
