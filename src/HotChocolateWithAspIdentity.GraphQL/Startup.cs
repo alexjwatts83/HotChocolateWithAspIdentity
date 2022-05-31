4e697dd51ae8200a2cfc6950d339e8db5d69d8d4
Alex Watts <alexjwatts83@gmail.com>
2022-06-01 06:59:56 +1000

    removed unusued usings

--- src/HotChocolateWithAspIdentity.GraphQL/Startup.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/Startup.cs
@@ -7,14 +7,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL
 {
-    public class Startup
+	public class Startup
     {
 		private readonly IConfiguration _configuration;
 		private readonly IWebHostEnvironment _environment;
