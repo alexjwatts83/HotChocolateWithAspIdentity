1e534ee869c5a9627b29c542a596e6014662a071
Alex Watts <alexjwatts83@gmail.com>
2022-06-05 07:33:34 +1000

    removed unused usings

--- src/HotChocolateWithAspIdentity.GraphQL/Startup.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/Startup.cs
@@ -2,15 +2,12 @@ using HotChocolateWithAspIdentity.Application.Interfaces;
 using HotChocolateWithAspIdentity.GraphQL.DependencyInjection;
 using HotChocolateWithAspIdentity.GraphQL.Services;
 using HotChocolateWithAspIdentity.Infrastructure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace HotChocolateWithAspIdentity.GraphQL
 {
