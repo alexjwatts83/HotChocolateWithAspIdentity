24d0b42129107c34231f24f3534e85be484e43f9
Alex Watts <alexjwatts83@gmail.com>
2022-06-08 07:42:23 +1000

    Added GraphQLServices

---
+++ src/HotChocolateWithAspIdentity.GraphQL/DependencyInjection/GrapQLServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotChocolateWithAspIdentity.GraphQL.DependencyInjection
+{
+	public static class GrapQLServices
+	{
+		public static IServiceCollection AddCustomGraphQL(
+			this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			return services;
+		}
+	}
+}
