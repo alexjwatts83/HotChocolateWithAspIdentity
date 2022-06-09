aed48e515e8ffde49c33d9c24f459dfa3c1da8f2
Alex Watts <alexjwatts83@gmail.com>
2022-06-10 07:29:02 +1000

    wip query file

---
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Query.cs
@@ -0,0 +1,24 @@
+﻿using HotChocolate;
+using HotChocolate.Data;
+using HotChocolateWithAspIdentity.Domain.Entities;
+using HotChocolateWithAspIdentity.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
+{
+	[GraphQLDescription("Represents the queries available.")]
+	public class Query
+	{
+		[UseDbContext(typeof(ApplicationDbContext))]
+		//[UseFiltering]
+		//[UseSorting]
+		[GraphQLDescription("Gets the queryable TodoLists.")]
+		public IQueryable<TodoList> GetPlatform([ScopedService] ApplicationDbContext context)
+		{
+			return context.TodoLists;
+		}
+	}
+}
