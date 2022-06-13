14685592f0e6c45e0d162ddce3be36d105e4bfc8
Alex Watts <alexjwatts83@gmail.com>
2022-06-14 08:30:24 +1000

    Add wip mutation

---
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Mutation.cs
@@ -0,0 +1,13 @@
+﻿using HotChocolate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
+{
+	[GraphQLDescription("Represents the mutations available.")]
+	public class Mutation
+	{
+	}
+}
