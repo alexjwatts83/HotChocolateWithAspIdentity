5f9046115a813286e7a5c3709444d91606f8678e
Alex Watts <alexjwatts83@gmail.com>
2022-06-14 08:34:50 +1000

    Added AddCommandInput

--- src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Mutation.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Mutation.cs
@@ -1,5 +1,7 @@
 ﻿using HotChocolate;
+using HotChocolate.Data;
 using HotChocolateWithAspIdentity.Domain.Entities;
+using HotChocolateWithAspIdentity.Infrastructure.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +9,26 @@ using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
 {
+	public record AddCommandInput(string Title, string Colour);
 	public record AddTodoListsPayload(TodoList TodoList);
 	[GraphQLDescription("Represents the mutations available.")]
 	public class Mutation
 	{
+		//[UseDbContext(typeof(ApplicationDbContext))]
+		//[GraphQLDescription("Adds a command.")]
+		//public async Task<AddTodoListsPayload> AddToListItemAsync(AddTodoListsPayload input, [ScopedService] ApplicationDbContext context)
+		//{
+		//	var command = new Command
+		//	{
+		//		HowTo = input.HowTo,
+		//		CommandLine = input.CommandLine,
+		//		PlatformId = input.PlatformId
+		//	};
 
+		//	context.Commands.Add(command);
+		//	await context.SaveChangesAsync();
+
+		//	return new AddCommandPayload(command);
+		//}
 	}
 }
