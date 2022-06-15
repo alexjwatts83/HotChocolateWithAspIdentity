49c34f8a1a31c1734c753e84e980cf559e5691d3
Alex Watts <alexjwatts83@gmail.com>
2022-06-16 07:58:38 +1000

    Added description to record

--- src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListInput.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListInput.cs
@@ -8,5 +8,6 @@ using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
 {
+	[GraphQLDescription("Input for adding a todo listAdde.")]
 	public record AddTodoListInput(string Title, string Colour, IEnumerable<AddTodoItemInput> Items);
 }
