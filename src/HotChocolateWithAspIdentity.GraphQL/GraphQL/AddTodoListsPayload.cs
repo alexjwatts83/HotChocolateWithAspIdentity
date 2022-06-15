2ff5e0a18333196289ceb8e13f20263af1b9f3ee
Alex Watts <alexjwatts83@gmail.com>
2022-06-16 07:57:45 +1000

    removed usings

--- src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListsPayload.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListsPayload.cs
@@ -1,13 +1,8 @@
 ﻿using HotChocolate;
-using HotChocolate.Data;
 using HotChocolateWithAspIdentity.Domain.Entities;
-using HotChocolateWithAspIdentity.Infrastructure.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
 {
-	[GraphQLDescription("Todo List Payload.")]
+	[GraphQLDescription("Todo List PayloadAdd.")]
 	public record AddTodoListsPayload(TodoList TodoList);
 }
