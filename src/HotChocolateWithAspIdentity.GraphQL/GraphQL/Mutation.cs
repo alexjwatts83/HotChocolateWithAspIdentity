706e9078e3897b871750e51794927976375a25b6
Alex Watts <alexjwatts83@gmail.com>
2022-06-16 07:55:14 +1000

    removed usings

--- src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Mutation.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/Mutation.cs
@@ -2,8 +2,6 @@
 using HotChocolate.Data;
 using HotChocolateWithAspIdentity.Domain.Entities;
 using HotChocolateWithAspIdentity.Infrastructure.Persistence;
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
