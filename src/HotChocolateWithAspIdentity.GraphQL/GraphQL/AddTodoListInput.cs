89db32b20d9ef7174fb186a0ba9b536f58bceb20
Alex Watts <alexjwatts83@gmail.com>
2022-06-20 07:51:05 +1000

    removed usings

--- src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListInput.cs
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoListInput.cs
@@ -1,10 +1,5 @@
 ﻿using HotChocolate;
-using HotChocolate.Data;
-using HotChocolateWithAspIdentity.Domain.Entities;
-using HotChocolateWithAspIdentity.Infrastructure.Persistence;
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
 {
