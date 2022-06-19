6e5d1657e87a809b632d5c7fbee9f5f1aa742f15
Alex Watts <alexjwatts83@gmail.com>
2022-06-20 07:53:59 +1000

    Added AddTodoItemPayload

---
+++ src/HotChocolateWithAspIdentity.GraphQL/GraphQL/AddTodoItemPayload.cs
@@ -0,0 +1,8 @@
+﻿using HotChocolate;
+using HotChocolateWithAspIdentity.Domain.Entities;
+
+namespace HotChocolateWithAspIdentity.GraphQL.GraphQL
+{
+	[GraphQLDescription("Add Todo List Item Payload.")]
+	public record AddTodoItemPayload(TodoItem TodoItem);
+}
